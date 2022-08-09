0ef159699432bbf134ffd68710d2b644fef182bd
Maya-Alferez <m.alferez17@gmail.com>
2022-08-09 16:32:05 -0500

    isesion
    scss a la página de isession,
    se agrega scss al footer, sin embargo este no aparece correctamente en las demás páginas, sólo en la principal,,,

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,20 +1,71 @@
-<h1 class="titulo-principal">
-    <a href="/" class="logo enlace">Mictli Gatita Pikachita</a>
-</h1>
-
-<form action="/" class="formulario" method="POST">
-    <div class="campo">
-        <label class="campo-label" for="email">email</label>
-        <input class="campo-field" type="email" id="email" placeholder="email" name="email">
-    </div>
-    <div class="campo">
-        <label class="campo-label" for="password">contraseña</label>
-        <input class="campo-field" type="password" id="password" placeholder="contraseña" name="password">
-    </div>
-    <input type="submit" class="boton" value="iniciar sesión">
-</form>
-
-<div class="acciones">
-    <a href="/crear">crear cuenta</a>
-    <a href="/forgot">iniciar sesión</a>
-</div>
+<html>
+    <body>
+        <h1 class="titulo-principal">
+            <a href="/" class="logo enlace">Mictli Gatita Pikachita</a>
+        </h1>
+        <form action="/" class="formulario" method="POST">
+            <fieldset>
+                <div class="contenedor-campos">
+                    <div class="campo">
+                        <input class="campo-field" type="email" id="email" placeholder="Nombre de usuario*" name="email">
+                    </div>
+                    <div class="campo">
+                        <input class="campo-field" type="password" id="password" placeholder="Contraseña*" name="password">
+                    </div>
+                    <div class="alinear-derecha flex">
+                        <input type="submit" class="boton w-sm-100" value="iniciar sesión">
+                    </div>
+                    <div class="acciones">
+                        <a href="/forgot">¿Has olvidado tú contraseña?</a>
+                    </div>
+                </div>
+            </fieldset>
+        </form>
+    </body>
+    
+    <footer class="footer">
+        <div class="redes">
+            <div class="red">
+                <ul class="social">
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <rect x="4" y="4" width="16" height="16" rx="4" />
+                                <circle cx="12" cy="12" r="3" />
+                                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pinterest" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <line x1="8" y1="20" x2="12" y2="11" />
+                                <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
+                                <circle cx="12" cy="12" r="9" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="reservado">
+        All Rights Reserved by <span class="lab">catsphereLabs</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-registered" width="16" height="16" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="9" />
+                <path d="M9 16v-8h4a2 2 0 0 1 0 4h-4m3 0l3 4" />
+            </svg> 
+            <span class="año">2022.</span>
+        </div>
+    </footer>
+</html>
--- views/auth/principal.php
+++ views/auth/principal.php
@@ -167,3 +167,49 @@
     </div> <!-- .contenedor-evees -->
 </section>
 </body>
+
+<footer class="footer">
+    <div class="redes">
+        <div class="red">
+            <ul class="social">
+                <li class="space-left">
+                    <a class="icono" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                        </svg>
+                    </a>
+                </li>
+                <li class="space-left">
+                    <a class="icono" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <rect x="4" y="4" width="16" height="16" rx="4" />
+                            <circle cx="12" cy="12" r="3" />
+                            <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                        </svg>
+                    </a>
+                </li>
+                <li class="space-left">
+                    <a class="icono" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pinterest" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <line x1="8" y1="20" x2="12" y2="11" />
+                            <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
+                            <circle cx="12" cy="12" r="9" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="reservado">
+    All Rights Reserved by <span class="lab">catsphereLabs</span>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-registered" width="16" height="16" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <circle cx="12" cy="12" r="9" />
+            <path d="M9 16v-8h4a2 2 0 0 1 0 4h-4m3 0l3 4" />
+        </svg> 
+        <span class="año">2022.</span>
+    </div>
+</footer>
