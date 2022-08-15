bcd85dea3a220b300af11c3950d377780980578e
Maya-Alferez <m.alferez17@gmail.com>
2022-08-15 16:50:38 -0500

    Sobre Mictli
    General el contenido de la página "Sobre Mictli", además de agregarle estilos scss en la página.

--- views/auth/sobremictli.php
+++ views/auth/sobremictli.php
@@ -1,1 +1,77 @@
-<h1>Sobre la gatita Mictli</h1>
+<html>
+    <body>
+        <h1 class="titulo-principal">
+            <a href="/" class="logo enlace">Mictli Gatita Pikachita</a>
+        </h1>
+        <div class="text-center">
+            <h2 class="titulo titulo-enlace"><a href="/">Inicio</a>/ Sobre Mictli</h2>
+        </div>
+
+        <section class="contenedor-sobre">
+            <div class="contenido-sobre">
+                <h2 class="titulo-pag">Sobre Mictli</h2>
+                <div class="contenido-texto">
+                    <p>Mictli Gatita Pikachita, es una gata de color negro con pancita rosita, la pareja conformada por Maya y Mando la adoptaron un 15 de enero del 2021.</p>
+                    <p>A Mictli le gusta mucho explorar su alrededores, no es muy activa sin embargo, le agrada jugar con hilitos (son sus favoritos), además de que el encanta comer, más no come cualquier cosa, sólo le gusta sus croquetas con agua fresca y muy de vez en cuando jamón serrano que generalmente le da su Karen macho Mando.</p>
+                    <p>Además de comer, a Mictli le gusta mucho dormir, sobre todo en la cama de sus karens, no es muy amigable con demás gatitos, ya que si ve uno, se asusta mucho y prefiere esconderse en vez de convivir con ellos.</p>
+                    <p>No se queda atrás su pasatiempo por ver videojuegos, ya que cuando sus karens se ponen a jugar, Mictli inmediatamente va con ellos para ver la pantalla y ver ahora qué están jugando, le gusta mucho ver cuando su karen macho Mando juega Fall Out, ya que sale un perrito que se llama albondiga.</p>
+                    <p>Mictli se convirtió en modelo de mascotas a la fuerza, impulsada por su Karen, Maya, es por eso que se le ve la cara de "enojación" en algunas imágenes del catalogo, pero eso no impide de que se vea súper bonita.</p>
+                    <p>La tienda virtual de Mictli se fundó en que los karens del mundo tengan ropita para sus mininos para que así les demuestren todo el amor que se les tiene, cabe destacar que cada prenda que se realiza en la tienda, es de manera artesana, por el cual los disfraces salen de inspiraciones de videojuegos que juegan sus karens.</p>
+                </div>
+            </div>
+            <div class="contenido-imagen">
+                <picture>
+                <source srcset="../build/img/Mictli.webp" type="image/webp">
+                <source srcset="../build/img/Mictli.jpg" type="image/jpeg">
+                <img loading="lazy" src="../build/img/Mictli.jpg" alt="sobre mictli">
+                </picture>
+            </div>
+        </section>
+    </body>
+
+    <footer class="footer">
+        <div class="redes">
+            <div class="red">
+                <ul class="social">
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <rect x="4" y="4" width="16" height="16" rx="4" />
+                                <circle cx="12" cy="12" r="3" />
+                                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pinterest" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <line x1="8" y1="20" x2="12" y2="11" />
+                                <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
+                                <circle cx="12" cy="12" r="9" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="reservado">
+            All Rights Reserved by <span class="lab">catsphereLabs</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-registered" width="16" height="16" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="9" />
+                <path d="M9 16v-8h4a2 2 0 0 1 0 4h-4m3 0l3 4" />
+            </svg> 
+            <span class="año">2022.</span>
+        </div>
+    </footer>
+</html>
