99745be8fee553edb1623a037dea841b1d096d1e
Maya-Alferez <m.alferez17@gmail.com>
2022-08-15 16:00:42 -0500

    Catalogo
    Se cambian el tamaño de las imágenes,

---
+++ views/catalogo/mimikyu.php
@@ -0,0 +1,130 @@
+<html>
+    <body>
+        <h1 class="titulo-principal">
+            <a href="/" class="logo enlace">Mictli Gatita Pikachita</a>
+        </h1>
+        <div class="text-center">
+            <h2 class="titulo titulo-enlace"><a href="/catalogo">Catalogo</a>/ Productos</h2>
+        </div>
+        
+        <section class="grid-contenedor">
+            <div class="contenedor-evees">
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/flareonmi.webp" type="image/webp">
+                        <source srcset="../build/img/flareonmi.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/flareonmi.jpg" alt="flareon mimikyu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Mimikyu flareon</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/jolteonmi.webp" type="image/webp">
+                        <source srcset="../build/img/jolteonmi.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/jolteonmi.jpg" alt="jolteon mimikyu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Mimikyu jolteon</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/sylveonmi.webp" type="image/webp">
+                        <source srcset="../build/img/sylveonmi.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/sylveonmi.jpg" alt="sylveon mimikyu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Mimikyu Sylveon</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/umbreonmi.webp" type="image/webp">
+                        <source srcset="../build/img/umbreonmi.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/umbreonmi.jpg" alt="umbreon mimikyu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Mimikyu Umbreon</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/vaporeonmi.webp" type="image/webp">
+                        <source srcset="../build/img/vaporeonmi.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/vaporeonmi.jpg" alt="vaporeon mimikyu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Mimikyu Vaporeon</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+            </div><!-- .contenedor-productos-->
+        </section>
+    </body>
+
+    <footer class="footer">
+        <div class="redes">
+            <div class="red">
+                <ul class="social">
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <rect x="4" y="4" width="16" height="16" rx="4" />
+                                <circle cx="12" cy="12" r="3" />
+                                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pinterest" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <line x1="8" y1="20" x2="12" y2="11" />
+                                <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
+                                <circle cx="12" cy="12" r="9" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="reservado">
+            All Rights Reserved by <span class="lab">catsphereLabs</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-registered" width="16" height="16" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="9" />
+                <path d="M9 16v-8h4a2 2 0 0 1 0 4h-4m3 0l3 4" />
+            </svg> 
+            <span class="año">2022.</span>
+        </div>
+    </footer>
+</html>
---
+++ views/catalogo/pikachu.php
@@ -0,0 +1,130 @@
+<html>
+    <body>
+        <h1 class="titulo-principal">
+            <a href="/" class="logo enlace">Mictli Gatita Pikachita</a>
+        </h1>
+        <div class="text-center">
+            <h2 class="titulo titulo-enlace"><a href="/catalogo">Catalogo</a>/ Productos</h2>
+        </div>
+        
+        <section class="grid-contenedor">
+            <div class="contenedor-evees">
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/charizard_mega.webp" type="image/webp">
+                        <source srcset="../build/img/charizard_mega.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/charizard_mega.jpg" alt="mega charizard pikachu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Pikachu Megacharizard</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/charizard.webp" type="image/webp">
+                        <source srcset="../build/img/charizard.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/charizard.jpg" alt="charizard pikachu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Pikachu charizard</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/gyarados.webp" type="image/webp">
+                        <source srcset="../build/img/gyarados.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/gyarados.jpg" alt="gyarados pikachu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Pikachu Gyarados</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/magickarp.webp" type="image/webp">
+                        <source srcset="../build/img/magickarp.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/magickarp.jpg" alt="magikarp pikachu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Pikachu Magikarp</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+
+                <div class="evee">
+                    <picture>
+                        <source srcset="../build/img/snorlax.webp" type="image/webp">
+                        <source srcset="../build/img/snorlax.jpg" type="image/jpeg">
+                        <img loading="lazy" src="../build/img/snorlax.jpg" alt="snorlax pikachu">
+                    </picture>
+                    <div class="contenido-evee">
+                        <h3 class="titulo-sec">Pikachu Snorlax</h3>
+                    </div>
+                    <div class="boton boton-cat">
+                        <a href="#">Ver detalles</a>
+                    </div><!--.boton -->
+                </div><!-- .producto -->
+            </div><!-- .contenedor-productos-->
+        </section>
+    </body>
+
+    <footer class="footer">
+        <div class="redes">
+            <div class="red">
+                <ul class="social">
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <rect x="4" y="4" width="16" height="16" rx="4" />
+                                <circle cx="12" cy="12" r="3" />
+                                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pinterest" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <line x1="8" y1="20" x2="12" y2="11" />
+                                <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
+                                <circle cx="12" cy="12" r="9" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="reservado">
+            All Rights Reserved by <span class="lab">catsphereLabs</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-registered" width="16" height="16" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="9" />
+                <path d="M9 16v-8h4a2 2 0 0 1 0 4h-4m3 0l3 4" />
+            </svg> 
+            <span class="año">2022.</span>
+        </div>
+    </footer>
+</html>
