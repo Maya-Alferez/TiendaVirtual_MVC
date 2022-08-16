0e67acc6a213bf933e8be4c5f8a606326a8f2248
Maya-Alferez <m.alferez17@gmail.com>
2022-08-16 13:04:35 -0500

    Términos y condiciones
    Contenido y diseño de la página términos y condiciones de Mictli gatita

--- views/auth/terminos.php
+++ views/auth/terminos.php
@@ -1,1 +1,80 @@
-<h1>Desde términos C:</h1>
+<html>
+    <body>
+        <h1 class="titulo-principal">
+            <a href="/" class="logo enlace">Mictli Gatita Pikachita</a>
+        </h1>
+        <div class="text-center">
+            <h2 class="titulo titulo-enlace"><a href="/">Inicio</a>/ Términos y condiciones</h2>
+        </div>
+
+        <section class="p-terminos">
+            <div class="p-titulo">
+                <p>Políticas, envíos y cambios</p>
+            </div>
+            <div class="contenedor-texto">
+                <p>Tienes 7 días después de recibir tú pedido para cualquier aclaración y cambio.</p>
+                <p>Se cobra una cuota por cambios de talla y errores del cliente de <span class="span-precio">$250.00 MXN.</span></p>
+                <p>No calcelamos órdenes después de que es procesado el pago, bajo ninguna circunstancia.</p>
+                <p>No hay cambios en las medidas a la hora de realizar el pedido.</p>
+                <p>No ofrecemos cambios a órdenes internacionales.</p>
+                <p>No hay devoluciones, pero si hay cambios en tallas. </p>
+                <p>Mictli Gatita no cubre el envío del cambio.</p>
+                <p>Si la prenda a cambiar tiene señales de haber sido usada, no habrá cambios.</p>
+                <p>Si una pieza esta dañada o con algún defecto, podemos ofrecerte un cambio.</p>
+                <p>Los cambios toman aprox tres semanas en realizarse a partir de que la prenda es recibida en el taller.</p>
+                <p>Para tu cambio te pedimos por favor que devuelvas la(s) pieza(s) en su embalaje original.</p>
+                <p>Mictli Gatita no cubre impuestos que puedan ser cobrados en envíos internacionales.</p>
+                <p>No realizamos pedidos personalizados. De hacerse algún cambio o pedido especial en la orden que no venga en nuestras opciones en la pagina web, no hay devoluciones ni cambios.</p>
+                <p>Los tiempos estimados cambiaran (aumentan) a partir de que realices algún cambio en tu orden.</p>
+                <p>No nos hacemos responsables de las entregas con la paquetería, es decir, si la dirección esta incorrecta, no había nadie en el domicilio o los números de contacto están incorrectos.</p>
+                <p class="last-p">Para más información por favor escribe a <span class="correo"><a href="#">cambios@mictligatita.com</a></span> e incluye tu nombre completo, numero de orden, prenda comprada y razón del cambio. El proceso del cambio se llevará únicamente por ese medio.</p>
+            </div>
+        </section>
+    </body>
+
+    <footer class="footer">
+        <div class="redes">
+            <div class="red">
+                <ul class="social">
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <rect x="4" y="4" width="16" height="16" rx="4" />
+                                <circle cx="12" cy="12" r="3" />
+                                <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+                            </svg>
+                        </a>
+                    </li>
+                    <li class="space-left">
+                        <a class="icono" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-pinterest" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <line x1="8" y1="20" x2="12" y2="11" />
+                                <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7" />
+                                <circle cx="12" cy="12" r="9" />
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="reservado">
+            All Rights Reserved by <span class="lab">catsphereLabs</span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-registered" width="16" height="16" viewBox="0 0 24 24" stroke-width="1" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <circle cx="12" cy="12" r="9" />
+                <path d="M9 16v-8h4a2 2 0 0 1 0 4h-4m3 0l3 4" />
+            </svg> 
+            <span class="año">2022.</span>
+        </div>
+    </footer>
+</html>
